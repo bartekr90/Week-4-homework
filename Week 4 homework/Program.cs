5823ee11583370a209409fa3e2245ad8a01a23ed
Bartosz Rachwał <bartekr90@gmail.com>
2021-12-17 21:11:48 +0100

    Letter validation ready

--- Week 4 homework/Program.cs
+++ Week 4 homework/Program.cs
@@ -7,35 +7,81 @@ namespace Week_4_homework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string kee;
+            while (true)
+            {
+
+                kee = LetterValidation(50, false);
+                Console.WriteLine(kee);
+            }
+
+
+
+
+
 
 
         }
-        private static string InputLimitation()
+        /// <summary>
+        /// Filters characters based on ASCII decimal values
+        /// </summary>
+        /// <param name="length">Limit length of string</param>
+        /// <param name="hide">Optional hiding of characters </param>
+        /// <param name="info">Console info after presing ENTER</param>
+        /// <returns></returns>
+        private static string LetterValidation(int length = 25, bool hide = false, string info = "")
         {
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
             bool loop = true;
+            char keySign;
+            int castSign;
+
             while (loop)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true); // won't show up in console
-                switch (keyInfo.Key)
+                ConsoleKeyInfo keyInfo = Console.ReadKey(hide);
+                keySign = keyInfo.KeyChar;
+                castSign = (int)keySign;
+
+                if (castSign == 27)
+                    goto Clear;
+
+                if (castSign == 8)
+                    goto Remove;
+
+                if (castSign == 13)
+                    goto ReturnString;
+
+                if (castSign == 45 || (castSign > 64 && castSign < 91) || (castSign > 96 && castSign < 123) || (castSign > 127 && castSign < 168))
+                    goto Append;
+
+                else
+                    goto NotAppend;
+
+                ReturnString:
+                loop = false;
+                Console.WriteLine(info);
+                break;
+
+            Append:
+                if (sb.Length < length)
                 {
-                    case ConsoleKey.Enter:
-                        {
-                            loop = false;
-                            Console.WriteLine();
-                            break;
-                        }
-                    default:
-                        {
-                            if (sb.Length < 25)
-                            {
-                                sb.Append(keyInfo.KeyChar);
-                                Console.Write(keyInfo.KeyChar);
-                            }
-                            break;
-                        }
+                    sb.Append(keySign);
                 }
+                continue;
+
+            NotAppend:
+                continue;
+
+            Remove:
+                if (sb.Length > 0)
+                    sb.Remove(sb.Length - 1, 1);
+                continue;
+
+            Clear:
+                sb.Clear();
+                continue;
             }
+
             return sb.ToString();
         }
 
