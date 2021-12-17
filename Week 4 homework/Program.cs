fe74a4e5da380fef2f98f0e88e007a3341ddd9a1
Bartosz Rachwał <bartekr90@gmail.com>
2021-12-17 17:27:59 +0100

    Add project files.

---
+++ Week 4 homework/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Week_4_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+        private static string InputLimitation()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            bool loop = true;
+            while (loop)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); // won't show up in console
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        {
+                            loop = false;
+                            Console.WriteLine();
+                            break;
+                        }
+                    default:
+                        {
+                            if (sb.Length < 25)
+                            {
+                                sb.Append(keyInfo.KeyChar);
+                                Console.Write(keyInfo.KeyChar);
+                            }
+                            break;
+                        }
+                }
+            }
+            return sb.ToString();
+        }
+
+    }
+}
